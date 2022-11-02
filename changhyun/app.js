996ab23950979e7e383fb7b96f92220b826e710d
julius <julius@juliusui-MacBookAir.local>
2022-11-02 21:55:47 +0900

    [수정]Express 초기 환경세팅

--- changhyun/app.js
+++ changhyun/app.js
@@ -1,53 +1,36 @@
-// 사용하는 npm 패키지(모듈)들 : express, nodemon, cors, dotenv, morgan
+require('dotenv').config();
 
 const express = require('express');
-const app = express();
 const cors = require('cors');
-const morgan = require('morgan'); //morgan 모듈 추가하기
-const dotenv = require('dotenv');
+const morgan = require('morgan');
+const { DataSource } = require('typeorm');
+const app = express();
 
-const { DataSource } = require('typeorm'); //DataSource 모듈을 불러오기 
+const PORT = process.env.PORT
 
-dotenv.config(); //dotenv 사용
-// ES 모듈에서 환경 변수 불러오기(import) 다시 공부하기.
-
-const myDataSource = new DataSource({
+const appDataSource = new DataSource({
   type: process.env.TYPEORM_CONNECTION,
   host: process.env.TYPEORM_HOST,
   port: process.env.TYPEORM_PORT,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE
-}) //process객체 : node.js 기본 글로벌 객체로 별도의 require 호출 없이 모든 모듈에서 접근이 가능 (process의 정확한 기능은 무엇인지?)
+})
 
-myDataSource.initialize()
+appDataSource.initialize()
   .then(() => {
       console.log("Data Source has been initialized!")
   })
+  .catch((error) => {
+    console.error(error);
+  })
 
 app.use(cors());
-app.use(morgan('combined')); //morgan 사용하기 (combined, common, dev, short, tiny)
+app.use(morgan('dev'));
+app.use(express.json());
 
-app.get('/ping', function (req, res, next){
-  res.json({message:'pong'});
+app.get("/ping", function (req, res, next) {
+  return res.status(200).json({ message : "pong" });
 })
-app.listen(3000, () => { console.log('server listening on port 3000')});
 
-
-/*  (morgan)
-로그 포맷 선택
-1. combined (로그 내용)
-:remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"
-
-2. common
-[:remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length]]
-
-3. dev 
-[:method :url :status :response-time ms - :res[content-length]]
-
-4. short
-[:remote-addr :remote-user :method :url HTTP/:http-version :status :res[content-length] - :response-time ms]
-
-5. tiny
-[:method :url :status :res[content-length] - :response-time ms]
-*/
+app.listen(PORT, () => { console.log(`server listening on port ${PORT}`)});
