66fe355c607176399e910671440c0c2fdd188729
IgnacioSEO <112772324+IgnacioSEO@users.noreply.github.com>
2022-11-03 20:56:44 +0900

    [서재선] Assignment 1:Express API - 초기 환경 세팅(수정)

--- jaeseon/app.js
+++ jaeseon/app.js
@@ -1,13 +1,15 @@
+require("dotenv").config();
+
 const http = require("http");
+
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const dotenv = require("dotenv");
-dotenv.config();
-
 const { DataSource } = require("typeorm");
 
-const myDataSource = new DataSource({
+const api = require("./../api");
+
+const appDataSource = new DataSource({
   type: process.env.TYPEORM_CONNECTION,
   host: process.env.TYPEORM_HOST,
   port: process.env.TYPEORM_PORT,
@@ -16,11 +18,11 @@ const myDataSource = new DataSource({
   database: process.env.TYPEORM_DATABASE,
 });
 
-myDataSource.initialize().then(() => {
+appDataSource.initialize().then(() => {
   console.log("Data Source has been initialized!");
 });
 
-app = express();
+const app = express();
 
 app.use(express.json());
 app.use(cors());
@@ -38,4 +40,3 @@ const start = async () => {
 };
 
 start();
-
