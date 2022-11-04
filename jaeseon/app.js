03c78527bb9f7ba5ae6f8594fe5cfd7636b4352a
IgnacioSEO <112772324+IgnacioSEO@users.noreply.github.com>
2022-11-04 11:26:38 +0900

    [서재선] Assignment 1:Express API - 초기 환경 세팅

--- jaeseon/app.js
+++ jaeseon/app.js
@@ -1,15 +1,14 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const http = require("http");
+const http = require('http');
 
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const { DataSource } = require("typeorm");
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const { DataSource } = require('typeorm');
 
-const api = require("./../api");
-
-const appDataSource = new DataSource({
+const api = require('./../api');
+const AppDataSource = new DataSource({
   type: process.env.TYPEORM_CONNECTION,
   host: process.env.TYPEORM_HOST,
   port: process.env.TYPEORM_PORT,
@@ -18,18 +17,21 @@ const appDataSource = new DataSource({
   database: process.env.TYPEORM_DATABASE,
 });
 
-appDataSource.initialize().then(() => {
-  console.log("Data Source has been initialized!");
-});
-
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
-app.get("/ping", (req, res) => {
-  return res.status(200).json({ message: "pong" });
+app.get('/ping', (req, res) => {
+  return res.status(200).json({ message: 'pong' });
 });
 
 const server = http.createServer(app);
--- jaeseon/db/migrations/20221103074949_create_users_signup_table.sql
+++ jaeseon/db/migrations/20221103074949_create_users_signup_table.sql
@@ -1,10 +1,10 @@
 -- migrate:up
 CREATE TABLE users_signup (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-email_id INT NOT NULL,
-password_id INT NOT NULL
-CONSTRAINT user_signup_email_id_fkey FOREIGN KEY (email_id) REFERENCES users_email(id),
-CONSTRAINT user_signup_password_id_fkey FOREIGN KEY (password_id) REFERENCES users_password(id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email_id INT NOT NULL,
+    password_id INT NOT NULL
+    CONSTRAINT user_signup_email_id_fkey FOREIGN KEY (email_id) REFERENCES users_email(id),
+    CONSTRAINT user_signup_password_id_fkey FOREIGN KEY (password_id) REFERENCES users_password(id)
 );
 -- migrate:down
 DROP TABLE users_signup;
