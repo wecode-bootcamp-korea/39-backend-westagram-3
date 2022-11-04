8479919ff9e0a1c944b2c735aa36552e03496420
julius <julius@juliusui-MacBookAir.local>
2022-11-04 19:57:29 +0900

    2

--- changhyun/db/migrations/20221103122905_create_posts_table.sql
+++ changhyun/db/migrations/20221103122905_create_posts_table.sql
@@ -1,12 +1,12 @@
 -- migrate:up
 CREATE TABLE posts (
-id INT NOT NULL AUTO_INCREMENT,
-title VARCHAR(100) NOT NULL,
-content TEXT NULL,
-user_id INT NOT NULL,
-created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-PRIMARY KEY (id),
-CONSTRAINT posts_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id)
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(100) NOT NULL,
+  content TEXT NULL,
+  user_id INT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  PRIMARY KEY (id),
+  CONSTRAINT posts_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 -- migrate:down
--- changhyun/db/migrations/20221103122926_create_likes_table.sql
+++ changhyun/db/migrations/20221103122926_create_likes_table.sql
@@ -1,12 +1,13 @@
 -- migrate:up
 CREATE TABLE likes (
-id INT NOT NULL AUTO_INCREMENT,
-user_id INT NOT NULL,
-post_id INT NOT NULL,
-created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-PRIMARY KEY (id),
-CONSTRAINT likes_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id),
-CONSTRAINT likes_post_id_fkey FOREIGN KEY (post_id) REFERENCES posts (id)
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  post_id INT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  PRIMARY KEY (id),
+  CONSTRAINT likes_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id),
+  CONSTRAINT likes_post_id_fkey FOREIGN KEY (post_id) REFERENCES posts (id),
+  CONSTRAINT unique_likes UNIQUE (user_id, post_id) 
 );
 
 -- migrate:down
