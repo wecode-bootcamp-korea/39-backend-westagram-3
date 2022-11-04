2493fbe9795b8c798314d1eea8e1d376c127b838
julius <julius@juliusui-MacBookAir.local>
2022-11-05 02:40:24 +0900

    [임창현]게시글 수정하기

--- changhyun/app.js
+++ changhyun/app.js
@@ -34,55 +34,61 @@ app.get("/ping", function (req, res, next) {
   return res.status(200).json({ message: "pong" });
 });
 
-app.post("/user", async (req, res) => {
-  const { name, email, profile_image, password } = req.body;
+app.post("/user/info", async (req, res, next) => {
+  const { name, email, profileImage, password } = req.body;
+
   await appDataSource.query(
     `INSERT INTO users(
       name,
       email,
       profile_image,
       password
-      ) VALUES (?, ?, ?, ?);
-      `,
-    [name, email, profile_image, password]
+    ) VALUES (?, ?, ?, ?);
+    `,
+    [name, email, profileImage, password]
   );
+
   res.status(201).json({ message: "userCreated" });
 });
 
-app.post("/post", async (req, res) => {
-  const { title, content, image, user_id } = req.body;
+app.post("/post", async (req, res, next) => {
+  const { title, content, imageUrl, userId } = req.body;
+
   await appDataSource.query(
     `INSERT INTO posts(
       title,
       content,
-      image,
+      image_url,
       user_id
-      ) VALUES (?, ?, ?, ?);
+    ) VALUES (?, ?, ?, ?);
       `,
-    [title, content, image, user_id]
+    [title, content, imageUrl, userId]
   );
+
   res.status(201).json({ message: "postCreated" });
 });
 
-app.get("/posts/view", async (req, res) => {
+app.get("/posts/view", async (req, res, next) => {
   await appDataSource.query(
     `SELECT
       users.id as userID,
       users.profile_image as userProfileImage,
       posts.id as postingId,
-      posts.image as postingImageUrl,
+      posts.image_url as postingImageUrl,
       posts.content as postingContent
-      FROM users 
-      INNER JOIN posts ON posts.user_id = users.id;
-      `,
+    FROM users 
+    INNER JOIN posts
+    ON posts.user_id = users.id;
+    `,
     (err, rows) => {
       res.status(200).json({ data: rows });
     }
   );
 });
 
-app.get("/user/post", async (req, res) => {
-  const { id } = req.body;
+app.get("/user/post/:id", async (req, res, next) => {
+  const { id } = req.params;
+
   const userInfo = await appDataSource.query(
     `SELECT
       users.id as userID,
@@ -102,6 +108,36 @@ app.get("/user/post", async (req, res) => {
   );
   userInfo[0]["postings"] = userPost;
   res.status(200).json({ data: userInfo });
+});
+
+app.patch("/post/:userId/:postId", async (req, res, next) => {
+  const { userId, postId } = req.params;
+  const { content } = req.body;
+  await appDataSource.query(
+    `UPDATE 
+        posts
+      SET content = ?
+      WHERE user_id = ${userId} and id=${postId};
+    `,
+    [content]
+  );
+
+  await appDataSource.query(
+    `SELECT
+      users.id as userId,
+      users.name as userName,
+      posts.id as postingId,
+      posts.title as postingTitle,
+      posts.content as postingContent
+    FROM users
+    INNER JOIN posts
+    ON users.id = posts.user_id
+    WHERE users.id = ${userId} and posts.id=${postId};
+    `,
+    (err, rows) => {
+      res.status(200).json({ data: rows });
+    }
+  );
 });
 
 app.listen(PORT, () => {
