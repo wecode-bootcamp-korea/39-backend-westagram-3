b95aa6d6291e9e6ed0dde933a8904d4dc41f7d09
IgnacioSEO <112772324+IgnacioSEO@users.noreply.github.com>
2022-11-07 19:26:58 +0900

    [서재선] Assignment 2: Westagram - 유저 회원가입 하기

--- jaeseon/app.js
+++ jaeseon/app.js
@@ -34,6 +34,20 @@ app.use(morgan('combined'));
 app.get('/ping', (req, res) => {
   return res.status(200).json({ message: 'pong' });
 });
+app.post('/users', async (req, res) => {
+  const { name, email, profile_image, password } = req.body;
+  await myDataSource.query(
+    `INSERT INTO users(
+      name,
+      email,
+      profile_image,
+      password
+    ) VALUES (?, ?, ?, ?);
+    `,
+    [name, email, profile_image, password]
+  );
+  res.status(201).json({ message: 'userCreated' });
+});
 
 const server = http.createServer(app);
 const PORT = process.env.PORT;
